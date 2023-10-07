243bcc1ae7494539db8dd0285da1222d76425baf
Vitaly <onebeattrue@gmail.com>
2023-10-07 22:50:37 +0300

    Заготовка макета

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
@@ -3,44 +3,36 @@ package edu.mirea.onebeattrue.instagramcardcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.mirea.onebeattrue.instagramcardcompose.ui.theme.InstagramCardComposeTheme
+import edu.mirea.onebeattrue.instagramcardcompose.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             InstagramCardComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    InstagramProfileCard()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InstagramCardComposeTheme {
-        Greeting("Android")
-    }
-}
