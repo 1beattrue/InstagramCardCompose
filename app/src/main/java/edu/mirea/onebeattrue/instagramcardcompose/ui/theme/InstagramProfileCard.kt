fa4cadd03f8240c12ff3303410090f404d12c019
Vitaly <onebeattrue@gmail.com>
2023-10-07 23:11:29 +0300

    Card - аналог CardView

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/ui/theme/InstagramProfileCard.kt
@@ -1,6 +1,8 @@
 package edu.mirea.onebeattrue.instagramcardcompose.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,23 +24,31 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp,
+        ),
+        border = BorderStroke(1.dp, Color.Black)
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .background(Color.Blue)
-                .size(50.dp)
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Blue)
+                    .size(50.dp)
+            ) {
 
+            }
+            TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
         }
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
     }
 }
 
