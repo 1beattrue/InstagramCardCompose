83e8bf2d9a61a947d8c4d75ce8553df928d35cf2
Vitaly <onebeattrue@gmail.com>
2023-10-07 23:37:39 +0300

    Работа с Theme

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/ui/theme/InstagramProfileCard.kt
@@ -2,7 +2,6 @@ package edu.mirea.onebeattrue.instagramcardcompose.ui.theme
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,9 +10,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,15 +21,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
 @Composable
 fun InstagramProfileCard() {
     Card(
+        modifier = Modifier
+            .padding(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
         shape = RoundedCornerShape(
             topStart = 4.dp,
             topEnd = 4.dp,
         ),
-        border = BorderStroke(1.dp, Color.Black)
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
     ) {
         Row(
             modifier = Modifier
@@ -74,5 +78,21 @@ private fun TwoBoxes() {
         ) {
 
         }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardLight() {
+    InstagramCardComposeTheme(darkTheme = false) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardDark() {
+    InstagramCardComposeTheme(darkTheme = true) {
+        InstagramProfileCard()
     }
 }
