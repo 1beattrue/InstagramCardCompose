b7bf62629ca4785d40bd81fb4bdc7fc922299943
Vitaly <onebeattrue@gmail.com>
2023-10-11 20:52:24 +0300

    Функция Image Основные моменты: modifier = Modifier.clip(CircleShape) - задание формы contentScale = ContentScale.Crop - разные варианты crop'а

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.mirea.onebeattrue.instagramcardcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,11 +11,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -38,4 +43,16 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun TestImage() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "простите, слепые люди",
+        modifier = Modifier.clip(CircleShape),
+        contentScale = ContentScale.Crop
+
+    )
 }
