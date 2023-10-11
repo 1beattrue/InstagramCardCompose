2e28afb0e59944ff4ad2a88c70f111f426820314
Vitaly <onebeattrue@gmail.com>
2023-10-11 21:35:25 +0300

    Порядок modifier'ов Порядок важен, можно использовать одинаковые методы несколько раз

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,6 +20,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
@@ -48,11 +52,22 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun TestImage() {
-    Image(
-        painter = painterResource(id = R.drawable.ic_launcher_background),
-        contentDescription = "простите, слепые люди",
-        modifier = Modifier.clip(CircleShape),
-        contentScale = ContentScale.Crop
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.Cyan)
+    ) {
+        Image(
+            modifier = Modifier
+                .background(Color.Green)
+                .padding(25.dp)
+                .size(100.dp)
+                .background(Color.Red)
+                .padding(25.dp),
+            painter = ColorPainter(Color.Yellow),
+            contentDescription = "простите, слепые люди",
+            contentScale = ContentScale.Crop
 
-    )
+        )
+    }
 }
