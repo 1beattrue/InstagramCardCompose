72df237910c64a504bd1d6897b29262265600ea7
Vitaly <onebeattrue@gmail.com>
2023-10-25 22:50:58 +0300

    Делегаты (теория)

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/ui/theme/InstagramProfileCard.kt
@@ -19,7 +19,12 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,7 +42,22 @@ import edu.mirea.onebeattrue.instagramcardcompose.R
 fun InstagramProfileCard(
     viewModel: MainViewModel
 ) {
-    val isFollowed = viewModel.isFollowing.observeAsState(false) // есть impl
+    val isFollowed = viewModel.isFollowing.observeAsState(false)
+
+    //**********************************************************************************************
+    // небольшой теоретический момент
+
+    val a: MutableState<Int> = rememberSaveable {
+        mutableIntStateOf(5)
+    }
+    a.value = 10
+
+    var b: Int by rememberSaveable { // использование делегатов (добавляются методы getValue() и setValue())
+        mutableIntStateOf(5)
+    }
+    b = 10
+
+    //**********************************************************************************************
 
     Card(
         modifier = Modifier
