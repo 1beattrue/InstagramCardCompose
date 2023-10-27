deea0785f99fe6756440f2826d19c6d374f5d385
Vitaly <onebeattrue@gmail.com>
2023-10-27 23:46:47 +0300

    Вставка различных типов элементов в LazyColumn

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
@@ -3,13 +3,17 @@ package edu.mirea.onebeattrue.instagramcardcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.movableContentOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.ViewModelProvider
 import edu.mirea.onebeattrue.instagramcardcompose.ui.theme.InstagramCardComposeTheme
 import edu.mirea.onebeattrue.instagramcardcompose.ui.theme.InstagramProfileCard
@@ -45,8 +49,26 @@ private fun Test(viewModel: MainViewModel) {
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
         ) {
-            LazyColumn {
-                repeat(10000) {
+            LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+                item {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_instagram),
+                        contentDescription = null
+                    )
+                }
+                repeat(5) {
+                    item {
+                        InstagramProfileCard(viewModel)
+                    }
+                }
+                item {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_instagram),
+                        contentDescription = null,
+
+                    )
+                }
+                repeat(5) {
                     item {
                         InstagramProfileCard(viewModel)
                     }
