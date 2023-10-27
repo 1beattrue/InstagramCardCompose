bdb11f1e71031fe70547d2a6a903b8c91a25df7e
Vitaly <onebeattrue@gmail.com>
2023-10-27 23:43:07 +0300

    Аналог RecyclerView: LazyColumn

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import edu.mirea.onebeattrue.instagramcardcompose.ui.theme.InstagramCardComposeTheme
@@ -21,13 +23,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramCardComposeTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background)
-                ) {
-                    InstagramProfileCard(viewModel)
+            Test(viewModel = viewModel)
+//            InstagramCardComposeTheme {
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(MaterialTheme.colorScheme.background)
+//                ) {
+//                    InstagramProfileCard(viewModel)
+//                }
+//            }
+        }
+    }
+}
+
+@Composable
+private fun Test(viewModel: MainViewModel) {
+    InstagramCardComposeTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            LazyColumn {
+                repeat(10000) {
+                    item {
+                        InstagramProfileCard(viewModel)
+                    }
                 }
             }
         }
