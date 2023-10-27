b277f228346e803952d26ebdd89e7bed2f717ddd
Vitaly <onebeattrue@gmail.com>
2023-10-28 01:59:20 +0300

    lazyListState (добавил возможность возврата к первому элементу)

--- app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/presentation/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/instagramcardcompose/presentation/MainActivity.kt
@@ -6,20 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowUp
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import edu.mirea.onebeattrue.instagramcardcompose.ui.theme.InstagramCardComposeTheme
 import edu.mirea.onebeattrue.instagramcardcompose.ui.theme.InstagramProfileCard
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -44,12 +55,31 @@ private fun Test(viewModel: MainViewModel) {
                 .background(MaterialTheme.colorScheme.background)
         ) {
             val models = viewModel.models.observeAsState(listOf())
-            LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+            val lazyListState = rememberLazyListState()
+            val scope = rememberCoroutineScope()
+            LazyColumn(
+                state = lazyListState
+            ) {
                 items(models.value) { model ->
                     InstagramProfileCard(
                         model = model,
                         onFollowedButtonClickListener = viewModel::changeFollowingStatus
                     )
+                }
+            }
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                FloatingActionButton(
+                    modifier = Modifier.padding(8.dp),
+                    onClick = {
+                        scope.launch {
+                            lazyListState.animateScrollToItem(0)
+                        }
+                    }
+                ) {
+                    Icon(Icons.Rounded.KeyboardArrowUp, contentDescription = null)
                 }
             }
         }
